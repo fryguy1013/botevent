6fc214c610d6bc957eca5b86697a2cbf370273de
Kevin Hjelden <fryguy@burntpopcorn.net>
2017-01-09 16:14:46 -0800

    Remove paypal link.

--- application/views/view_event_registration.php
+++ application/views/view_event_registration.php
@@ -27,22 +27,6 @@
 			</form>
 		</div>
 		
-		<div class="pay_by_paypal">
-			Pay with paypal
-			<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit">
-			<input type="hidden" name="add" value="1">
-			<input type="hidden" name="cmd" value="_cart">
-			<input type="hidden" name="business" value="paypal@robogames.net">
-			<input type="hidden" name="item_name" value="Registration Fees for <?=htmlentities($registration->teamname)?> ">
-			<input type="hidden" name="amount" value="<?=htmlentities($registration->due)?>">
-			<input type="hidden" name="no_note" value="1">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="return" value="http://robogames.net/registration/event_registration/view/<?=htmlentities($registration->id)?>">
-			<input type="hidden" name="currency_code" value="USD">
-			</form>
-		</div>
-		
 		<? elseif ($registration->status == "accepted"): ?>
 		Your registration has been accepted, and you have paid. See you at the
 		event!
